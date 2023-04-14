e351a1219f7a4ea92b9c410f5425ace68a59cfca
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-04-14 21:59:58 +0530

    modified addTodo function

--- src/MyComponents/AddTodo.js
+++ src/MyComponents/AddTodo.js
@@ -1,55 +1,75 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 
 import React, { useState } from 'react';
-import  myImage  from "./logo1.png";
-import "./cssComp/style.css";
-import { PlusOutlined } from '@ant-design/icons';
-
-export const AddTodo = ({addTodo}) => {
-  const [title, setTitle] = useState("");
-  const [desc, setDesc] = useState("");
+// import  myImage  from "./logo1.png";
+import './cssComp/style.css';
 
 
-  const add = (event) => {
-      event.preventDefault();
-      if (!title || !desc) {
-          alert("Title or Description cannot be blank");
-      }
-      else {
-          addTodo(title, desc);
-          setTitle("");
-          setDesc("");
-      }
-  }
+export const AddTodo = ({ addTodo, todo, setTodo }) => {
+	const add = (event) => {
+		event.preventDefault();
+		const { id, title, desc } = todo;
+		if (!title || !desc) {
+			alert('Title or Description cannot be blank');
+		} else {
+			addTodo(id, title, desc);
+			setTodo({
+				title: '',
+				desc: '',
+			});
+		}
+	};
 
- 
-  return (
-    <>
-    <div className="addTodoContainer">
+	const handleChange = (event) => {
+		const { name } = event.target;
+		// console.log(name);
+		setTodo((elem) => {
+			return { ...elem, [name]: event.target.value };
 
 
-     <img src={myImage} alt="My Image"  style={{ display: 'block', margin: '0 auto', width:"20vh"}} />
-      <div className="container formContainer my-5">
-        <form onSubmit={add} className='form'>
-          <div className="mb-3">
-            <label htmlFor="title" className="form-label">
-              Task Title
-            </label>
-            <input type="text" value={title} onChange={(event) => setTitle(event.target.value)} className="form-control" id="addTitle" />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="desc" className="form-label">
-             Sub Task
-            </label>
-            <input type="text" value={desc} onChange={(event) => setDesc(event.target.value)} className="form-control" id="desc" />
-          </div>
+			
+		});
+	};
 
-          <button type="submit" className="addBtn">
-         <p><PlusOutlined /></p> 
-          </button>
-        </form>
-      </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="addTodoContainer">
+				{/* <img src={myImage} alt="My Image"  style={{ display: 'block', margin: '0 auto', width:"20vh"}} /> */}
+				<div className="container formContainer my-5">
+					<form onSubmit={add} className="form">
+						<div className="mb-3">
+							<label htmlFor="title" className="form-label">
+								Task Title
+							</label>
+							<input
+								type="text"
+								name="title"
+								value={todo.title}
+								onChange={handleChange}
+								className="form-control"
+								id="addTitle"
+							/>
+						</div>
+						<div className="mb-3">
+							<label htmlFor="desc" className="form-label">
+								Sub Task
+							</label>
+							<input
+								type="text"
+								name="desc"
+								value={todo.desc}
+								onChange={handleChange}
+								className="form-control"
+								id="desc"
+							/>
+						</div>
+
+						<button type="submit" className="addBtn">
+							+
+						</button>
+					</form>
+				</div>
+			</div>
+		</>
+	);
 };
