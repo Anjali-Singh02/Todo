183ca28348fa362aa52a06ef8868c446f447fa8a
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-04-14 22:04:34 +0530

    added edit button functionality

--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -3,20 +3,22 @@ import "./cssComp/style.css";
 import { DeleteFilled } from "@ant-design/icons";
 import { EditFilled } from "@ant-design/icons";
 export const TodoItem = ({ todo, onDelete, onEdit }) => {
+  // console.log(todo);
   return (
+
     <>
       {/* {props.todo} */}
-      <div className="container itemContainer">
-        <h4>{todo.title}</h4>
-        <p className="px-3">{todo.desc}</p>
-        <div className="btnContainer">
-          <button
-            className="btn btn-outline-light px-2 py-1 mx-2"
-            onClick={() => {
-              onDelete(todo);
-            }}
-          >
-            <DeleteFilled />
+  <div className="container itemContainer">
+    <h4>{todo.title}</h4>
+    <p className="px-3">{todo.desc}</p>
+    <div className="btnContainer">
+      <button
+        className="btn btn-outline-light px-2 py-1 mx-2"
+        onClick={() => {
+          onDelete(todo);
+        }}
+      >
+        <DeleteFilled />
           </button>
           <button
             className="btn btn-outline-light px-2 py-1"
