6bc33b8c3ed16aff0bf82ed827e6b8eecf63df6b
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-04-14 22:06:17 +0530

    applied css

--- src/MyComponents/TodoList.js
+++ src/MyComponents/TodoList.js
@@ -1,47 +1,47 @@
-import React from "react";
-import { TodoItem } from "./TodoItem";
-import "./cssComp/style.css";
+import React from 'react';
+import { TodoItem } from './TodoItem';
+import './cssComp/style.css';
 
-export const TodoList = (props) => {
-  return (
-    <>
-      <div className="container todoContainer">
-        <p>
-          <button
-            className="todoBtn"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapseWidthExample"
-            aria-expanded="false"
-            aria-controls="collapseWidthExample"
-          >
-          Todo List
-          </button>
-        </p>
-        <div>
-          <div className="collapse" id="collapseWidthExample">
-            <div className="card card-body">
-              <div className="container listContainer ">
-                {props.todos.length === 0
-                  ? "No todos to display"
-                  : props.todos.map((todo) => {
-                      return (
-                        <>
-                        <TodoItem
-                          todo={todo}
-                          key={todo.sno}
-                          onDelete={props.onDelete}
-                        />
-                        <hr />
-                        </>
-
-                      );
-                    })}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+export const TodoList = ({ todos, onDelete, onEdit }) => {
+	return (
+		<>
+			<div className="container todoContainer">
+				<p>
+					<button
+						className="todoBtn"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#collapseWidthExample"
+						aria-expanded="false"
+						aria-controls="collapseWidthExample"
+					>
+						Todo List
+					</button>
+				</p>
+				<div>
+					<div className="collapse" id="collapseWidthExample">
+						<div className="card card-body">
+							<div className="container listContainer ">
+								{todos.length === 0
+									? 'No todos to display'
+									: todos.map((todo, ind) => {
+											return (
+												<>
+													<TodoItem
+														todo={todo}
+														key={ind}
+														onDelete={onDelete}
+														onEdit={onEdit}
+													/>
+													<hr />
+												</>
+											);
+									  })}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
