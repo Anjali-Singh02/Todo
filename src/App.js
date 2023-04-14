209208b473469771b5a1c9e1ce11241b9763fbcb
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-04-14 21:58:32 +0530

    updated onEdit function

--- src/App.js
+++ src/App.js
@@ -1,74 +1,101 @@
-import "./App.css";
-import { Header } from "./MyComponents/Header";
-import { TodoList } from "./MyComponents/TodoList";
-import { AddTodo } from "./MyComponents/AddTodo";
-import { Features } from "./MyComponents/Features";
-
+import './App.css';
+import { Header } from './MyComponents/Header';
+import { TodoList } from './MyComponents/TodoList';
+import { AddTodo } from './MyComponents/AddTodo';
+import { Features } from './MyComponents/Features';
 
 // import { Footer } from "./MyComponents/Footer";
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { randomNumber } from './MyComponents/lib/lib';
 
 function App() {
-  let initTodo;
-  if (localStorage.getItem("todos") === null) {
-    initTodo = [];
-  } else {
-    initTodo = JSON.parse(localStorage.getItem("todos"));
-  }
+	let initTodo;
+	if (localStorage.getItem('todos') === null) {
+		initTodo = [];
+	} else {
+		initTodo = JSON.parse(localStorage.getItem('todos'));
+	}
 
-  const onDelete = (todo) => {
-    console.log("Delete", todo);
-    setTodos(
-      todos.filter((e) => {
-        return e !== todo;
-      })
-    );
+	const onDelete = (todo) => {
+		console.log('Delete', todo);
+		setTodos(
+			todos.filter((e) => {
+				return e !== todo;
+			}),
+		);
 
-    localStorage.setItem("todos", JSON.stringify(todos));
-  };
-  const addTodo = (title, desc) => {
-    console.log("I am adding this todo", title, desc);
-    let sno;
-    if (todos.length === 0) {
-      sno = 0;
-    } else {
-      sno = todos[todos.length - 1].sno + 1;
-    }
-    const myTodo = {
-      sno: sno,
-      title: title,
-      desc: desc,
-    };
-    setTodos([...todos, myTodo]);
-    console.log(myTodo);
-  };
+		localStorage.setItem('todos', JSON.stringify(todos));
+	};
+	const addTodo = (id, title, desc) => {
+		console.log('I am adding this todo', id, title, desc);
+		console.log(todos);
+		if (todos.some((elem) => elem.id === id)) {
+			const result = todos.find((elem) => elem.id === id);
+			console.log(result);
 
-  const [todos, setTodos] = useState(initTodo);
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
+			setTodos(
+				todos.map((elem) => {
+					if (elem.id === id) {
+						return {
+							id: id,
+							title: title,
+							desc: desc,
+						};
+					}
+					return elem;
+				}),
+			);
+		} else {
+			const myTodo = {
+				id: randomNumber,
+				title: title,
+				desc: desc,
+			};
+			setTodos([...todos, myTodo]);
+			// console.log(myTodo);
+		}
+	};
+	const [todos, setTodos] = useState(initTodo);
+	useEffect(() => {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}, [todos]);
 
-  return (
-    <>
-      <BrowserRouter>
-        <Header title="Todo List" />
-       
-        <Routes>
-          <Route
-            path="/"
-            element={<><AddTodo addTodo={addTodo} /><TodoList todos={todos} onDelete={onDelete} /></>
-            }
-          ></Route>
+	const [todo, setTodo] = useState({ id: randomNumber, title: '', desc: '' });
 
-          <Route path="/features" element={<Features />}>
-         
-          </Route>
-        </Routes>
-      </BrowserRouter>
-      {/* <Footer /> */}
-    </>
-  );
+	const onEdit = (value) => {
+		console.log(value);
+		setTodo({ id: value.id, title: value.title, desc: value.desc });
+	};
+
+	return (
+		<>
+			<BrowserRouter>
+				<Header title="Todo List" />
+				<Routes>
+					<Route
+						path="/"
+						element={
+							<>
+								<AddTodo
+									addTodo={addTodo}
+									todo={todo}
+									setTodo={setTodo}
+								/>
+								<TodoList
+									todos={todos}
+									onDelete={onDelete}
+									onEdit={onEdit}
+								/>
+							</>
+						}></Route>
+
+					<Route path="/features" element={<Features />}></Route>
+				</Routes>
+			</BrowserRouter>
+			{/* <Footer /> */}
+		</>
+	);
 }
 
 export default App;
