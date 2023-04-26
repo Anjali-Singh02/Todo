5a258c6452e7187d951a1470bd16eff751406bfc
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-04-27 00:16:13 +0530

    added checkbox

--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -1,35 +1,34 @@
-import React from "react";
-import "./cssComp/style.css";
-import { DeleteFilled } from "@ant-design/icons";
-import { EditFilled } from "@ant-design/icons";
+import React from 'react';
+import './cssComp/style.css';
+import { DeleteFilled } from '@ant-design/icons';
+import { EditFilled } from '@ant-design/icons';
 export const TodoItem = ({ todo, onDelete, onEdit }) => {
-  // console.log(todo);
-  return (
+	// console.log(todo);
+	return (
+		<>
+			{/* {props.todo} */}
+			<div className="container itemContainer">
+				<h4>{todo.title}</h4>
+				<p className="px-3">{todo.desc}</p>
+				<div className="btnContainer">
+					<DeleteFilled
+						className="btn btn-outline-light px-2 py-1 mx-2"
+						onClick={() => {
+							onDelete(todo);
+						}}
+					/>
 
-    <>
-      {/* {props.todo} */}
-  <div className="container itemContainer">
-    <h4>{todo.title}</h4>
-    <p className="px-3">{todo.desc}</p>
-    <div className="btnContainer">
-      <button
-        className="btn btn-outline-light px-2 py-1 mx-2"
-        onClick={() => {
-          onDelete(todo);
-        }}
-      >
-        <DeleteFilled />
-          </button>
-          <button
-            className="btn btn-outline-light px-2 py-1"
-            onClick={() => {
-              onEdit(todo);
-            }}
-          >
-            <EditFilled />
-          </button>
-        </div>
-      </div>
-    </>
-  );
+					<EditFilled
+						className="btn btn-outline-light px-2 py-1"
+						onClick={() => {
+							onEdit(todo);
+						}}
+					/>
+					<span className="checkBox">
+						<input type="checkbox" />
+					</span>
+				</div>
+			</div>
+		</>
+	);
 };
